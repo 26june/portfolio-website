a6915d456ab6480f6b12df681e659da333ed5a04
26june <gj.abordo1@gmail.com>
2023-01-08 15:22:21 +0000

    create navbar

---
+++ src/Components/common/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../Styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      <Link className="home-button" to={"/"}>
+        {/* <span className="home-button-icon"></span> */}
+        <span className="material-symbols-outlined">home</span>
+        <p>HOME</p>
+      </Link>
+
+      <Link className="skills-button" to={"/skills"}>
+        <span className="material-symbols-outlined">settings</span>
+        <p>SKILLS</p>
+      </Link>
+
+      <Link className="project-button" to={"/projects"}>
+        <span className="material-symbols-outlined">folder_open</span>
+        <p>PROJECTS</p>
+      </Link>
+    </div>
+  );
+}
