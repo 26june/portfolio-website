34e84e3416d066f1c583e82914c9a08abcde956c
26june <gj.abordo1@gmail.com>
2023-01-09 14:05:42 +0000

    update projects page

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,7 +5,52 @@ import "../Styles/Projects.css";
 export default function Projects() {
   return (
     <div className="Projects">
-      <Navbar></Navbar>
+      <Navbar currentPage={"projects"}></Navbar>
+
+      <div className="projects-list">
+        <a
+          href="https://github.com/26june/nc-games-project"
+          className="single-project"
+          target={"blank"}
+        >
+          <h2>NC HOUSE OF GAMES API</h2>
+          <p>
+            A backend service created using node.js, express and postegreSQL
+          </p>
+        </a>
+        <a
+          href="https://github.com/26june/nc-games-project-frontend"
+          className="single-project"
+          target={"blank"}
+        >
+          <h2>NC HOUSE OF GAMES FRONTEND</h2>
+          <p>
+            A frontend for my backend application. Used Axios for my HTTP
+            requests, built using React styled with Google's MUI library.
+          </p>
+        </a>
+        <a
+          href="https://github.com/26june/walkerrr"
+          className="single-project"
+          target={"blank"}
+        >
+          <h2>WALKERRR</h2>
+          <p>
+            A mobile application that gamifies your walks. You complete quests
+            by walking which rewards you with coins that you can use to buy
+            items in the shop. The tech stack consists of Flutter, MongoDB,
+            Firebase Auth.
+          </p>
+        </a>
+        <a
+          href="https://github.com/26june/test-wordleV2"
+          className="single-project"
+          target={"blank"}
+        >
+          <h2>WORDLE CLONE</h2>
+          <p>A clone of the popular game Wordle created for React practice.</p>
+        </a>
+      </div>
     </div>
   );
 }
